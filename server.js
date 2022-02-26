9428bcafa26cbdd1b2031c672c18064d53e0c30d
DanielGalstyan <91192182+DanielGalstyan@users.noreply.github.com>
2022-02-26 20:27:57 +0400

    express server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//test
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/post", (req, res) => {
+  res.send("this is post request");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
