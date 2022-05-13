d7d4f2a402b673ffbb13222328193cbcb2378d33
DanielGalstyan <91192182+DanielGalstyan@users.noreply.github.com>
2022-05-13 21:09:31 +0400

    Cart

--- Server/routes/Cart.js
+++ Server/routes/Cart.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Cart = require("../models/Cart");
+const { route } = require("./Auth");
 
 router.post("/add", async (req, res) => {
   try {
@@ -12,4 +13,61 @@ router.post("/add", async (req, res) => {
   }
 });
 
+//update cart data
+
+router.put("/cartUpdate", async (req, res) => {
+  try {
+    const updateCart = await Cart.findByIdAndUpdate(
+      req.body.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateCart);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//cart deleting
+
+router.delete("/cartdeleting", async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.body.id);
+    res.status(200).json("Cart has been deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// find by user
+router.post("/finduser", async (req, res) => {
+  try {
+    const userCart = await Cart.findOne({
+      userId: req.body.userId,
+    });
+    res.status(200).json(userCart);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// Get all carts
+
+router.post("/getcarts", async (req, res) => {
+  try {
+    const getCarts = await Cart.find();
+    console.log(getCarts);
+    res.status(200).json(getCarts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
